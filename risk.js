8e18a56c82aecfbe2515481cbf3d323526ccfa38
Evan Bedser <ebedser@stevens.edu>
2016-09-02 13:29:26 -0400

    switched from hard to soft tabs (spaces)

--- risk.js
+++ risk.js
@@ -4,38 +4,38 @@
 var countryList = [] //list of all the country objects
 
 function Country(name){
-	this.name = name //name of the country
-	this.connectedTo = [] //connected countries
-	this.ownedBy = "" //player that owns the country (blank by default)
-	this.population = 0 //how many units a player has there (0 by default)
-	countryList[countryList.length] = this //adds country to the list
+  this.name = name //name of the country
+  this.connectedTo = [] //connected countries
+  this.ownedBy = "" //player that owns the country (blank by default)
+  this.population = 0 //how many units a player has there (0 by default)
+  countryList[countryList.length] = this //adds country to the list
 
-	this.connectBoth = function(otherCountries){ // standard connect preffered
-		for(i=0; i<otherCountries.length; i++){
-			this.connectedTo[this.connectedTo.length] = otherCountries[i]
-			otherCountries[i].connectedTo[otherCountries[i].connectedTo.length]=this
-		}
-	}
+  this.connectBoth = function(otherCountries){ // standard connect preffered
+    for(i=0; i<otherCountries.length; i++){
+      this.connectedTo[this.connectedTo.length] = otherCountries[i]
+      otherCountries[i].connectedTo[otherCountries[i].connectedTo.length]=this
+    }
+  }
 
-	this.connect = function(otherCountries){ //preffered method of connecting
-		for(i=0; i<otherCountries.length; i++){
-			this.connectedTo[this.connectedTo.length] = otherCountries[i]
-		}
-	}
-	this.popMod = function(int){ //proper method for changing population
-		this.population+=int;
-		if (this.population < 0){this.population = 0}
-	}
+  this.connect = function(otherCountries){ //preffered method of connecting
+    for(i=0; i<otherCountries.length; i++){
+      this.connectedTo[this.connectedTo.length] = otherCountries[i]
+    }
+  }
+  this.popMod = function(int){ //proper method for changing population
+    this.population+=int;
+    if (this.population < 0){this.population = 0}
+  }
 }
 
 var playerList = []
 
 function Player(name){
-	this.name = name
-	playerList[playerList.length] = this
+  this.name = name
+  playerList[playerList.length] = this
 }
 
-//adding some countries to test
+//adding some countries to test (keep for later)
 Indonesia = new Country("Indonesia")
 NewGuinea = new Country("New Guinea")
 WesternAustralia = new Country("Western Australia")
